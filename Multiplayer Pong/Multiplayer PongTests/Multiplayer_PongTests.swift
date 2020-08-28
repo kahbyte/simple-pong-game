fd723f9e9639ac0886838087db7c94de25c67530
kahbyte <kaue.sbsousa@gmail.com>
2020-08-28 16:58:21 -0300

    projeto inicial

---
+++ Multiplayer Pong/Multiplayer PongTests/Multiplayer_PongTests.swift
@@ -0,0 +1,34 @@
+//
+//  Multiplayer_PongTests.swift
+//  Multiplayer PongTests
+//
+//  Created by Kauê Sales on 28/08/20.
+//  Copyright © 2020 Kauê Sales. All rights reserved.
+//
+
+import XCTest
+@testable import Multiplayer_Pong
+
+class Multiplayer_PongTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
